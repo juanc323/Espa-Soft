28190ba304cadfeb90ad850742f093e4c22e5c59
juanc323 <49367088+juanc323@users.noreply.github.com>
2021-05-01 05:30:22 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+document.querySelector('.menu-btn').addEventListener('click', () =>{
+    document.querySelector('.nav-menu').classList.toggle('show');
+})
+document.ready
+
+
+
+const ulSquares = document.querySelector('ul.squares');
+
+for ( let i = 0; i < 15; i++ ) {
+  const li = document.createElement('li');
+
+  const random = (min, max) => Math.random() * (max - min) + min
+  
+  const size = Math.floor(random(10, 120));
+  const position1 = random(68, 99);
+  const position2 = random(1, 32);
+  i == 0 ? delay = 1 : delay = random(5, 0.1);
+  const duration = random(24, 12);
+  let position = Number
+  if (i % 2 == 0) {
+    position = position1;
+  } else {
+    position = position2
+    li.style.animationDirection = 'reverse';
+  }
+
+  li.style.width = `${size}px`;
+  li.style.height = `${size}px`;
+  li.style.bottom = `-${size}px`;
+  li.style.left = `${position}%`;
+  li.style.animationDelay = `${delay}s`;
+  li.style.animationDuration = `${duration}s`;
+  li.style.animationTimingFunction = `cubic-bezier: ${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()}`;
+
+  ulSquares.appendChild(li);
+}
+
+
+  const questions = document.querySelectorAll(".question")
+  questions.forEach((question) => question.addEventListener("click", () =>{
+
+      if(question.parentNode.classList.contains("active")){
+          question.parentNode.classList.toggle("active")
+      }
+  else{
+  questions.forEach(question => question.parentNode.classList.remove("active"))
+  question.parentNode.classList.add("active")
+      }
+
+  }))
+ 
